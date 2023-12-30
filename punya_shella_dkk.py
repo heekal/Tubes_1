326ce8e7f89948be43bbabeeb3292b97752b334b
Haikal Ali <haikalali@student.telkomuniversity.ac.id>
2023-12-30 21:04:52 +0700

    aplikasi don

--- punya_shella_dkk.py
+++ punya_shella_dkk.py
@@ -1,4 +1,4 @@
-# Admin
+# Database
 admin = {
     'username' : ['admin1', 'admin2'],
     'password' : ['123', '321']
@@ -25,10 +25,10 @@ pegawai = {
     'divisi' : ['pejabat', 'pejabat', 'database', 'database']
 }
 
-
+# Parent Class
 class Aplikasi:
-    def __init__(self, nama, password, jabatan):
-        self.nama = nama
+    def __init__(self, username, password, jabatan):
+        self.username = username
         self.password = password
         self.jabatan = jabatan
 
@@ -42,9 +42,54 @@ class Aplikasi:
         print('Silakan Pilih Opsi Di Bawah Ini: ')
         print('1. Tambah/Edit Direktur SDM')
         print('2. Tambah/Edit Direktur')
+        print('3. Keluar')
         
     def sdm_menu(self):
         print('Silakan Pilih Opsi Di Bawah Ini: ')
-        print('1. Tambah/Edit Pegawai')
-        print('2. Tambah/Edit Gaji Pokok dan Gaji Per Jam')
-        print('3. ')
+        print('1. Tambah/Edit Data') # ini bisa edit data pegawai atau gaji
+        print('2. Lihat Semua Pegawai')
+        print('3. Lihat Pegawai Tertentu') # ini bisa dari bulan atau divisi
+        print('4. Lihat Divisi') # tampilin Standar Gaji
+        print('5. Lihat Pejabat Kantor')
+        print('6. Hitung Total Gaji Yang Perlu Dibayar')
+        print('7. Keluar')
+
+    def direktur_menu(self):
+        print('Silakan Pilih Opsi Di Bawah Ini: ')
+        print('1. Lihat Semua Pegawai')
+        print('2. Lihat Pegawai Tertentu') # ini bisa dari bulan atau divisi
+        print('3. Lihat Divisi') # tampilin Standar Gaji
+        print('4. Lihat Pejabat Kantor')
+        print('5. Hitung Total Gaji Yang Perlu Dibayar')
+        print('6. Keluar')
+
+    def verifikasi(self):
+        if self.nama in self.jabatan['username'] and self.password in self.jabatan['password']:
+            return True
+        return False
+    
+class Admin(Aplikasi):
+    def __init__(self, username, password, jabatan):
+        super().__init__(username, password, jabatan)
+
+    def main_menu(self):
+        Admin.admin_menu(self)
+        opsi = int(input('Pilihan Kamu: '))
+
+        if opsi == 1:
+            self.tambah_data(self, direktur_sdm)
+        elif opsi == 2:
+            self.tambah_data(self, direktur)
+        elif opsi == 3:
+            return False
+        
+    def tambah_data(self, tujuan):
+        tujuan['username'].append(input('Masukkan Nama Baru: '))
+        tujuan['password'].append(input('Masukkan Password Baru :'))
+        print('Data Telah Ditambahkan !')
+
+    def edit_data(self, tujuan):
+        username_cari = input('Masukkan Username Yang Ingin Di cari : ')
+        password_cari = input('Masukkan Password Baru : ')
+        if username_cari in tujuan['username'] and password_cari in tujuan['password']:
+            
